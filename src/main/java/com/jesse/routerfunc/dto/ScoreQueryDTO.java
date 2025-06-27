fee93180c8db3de7b4890f6802f521a9572cfef6
浩进 朱 <3191955858@qq.com>
2025-06-27 14:05:50 +0800

    成绩查询操作 DTO。

--- src/main/java/com/jesse/routerfunc/dto/ScoreQueryDTO.java
+++ src/main/java/com/jesse/routerfunc/dto/ScoreQueryDTO.java
@@ -1,13 +1,13 @@
 package com.jesse.routerfunc.dto;
 
 import lombok.*;
-import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
 
 @Data
+@ToString
 @NoArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(exclude = "scoreId")
 public class ScoreQueryDTO
 {
@@ -15,24 +15,20 @@ public class ScoreQueryDTO
     private Integer scoreId;
 
     // 用户 ID
-    private @NonNull Integer userId;
+    private Long userId;
 
     // 用户名
-    private @NotNull String userName;
+    private String userName;
 
     // 成绩提交日期
-    private @NonNull
-    LocalDateTime submitDate = LocalDateTime.now();
+    private LocalDateTime submitDate = LocalDateTime.now();
 
     // 正确数
-    private @NonNull
-    Integer correctCount = 0;
+    private Integer correctCount = 0;
 
     // 错误数
-    private @NonNull
-    Integer errorCount = 0;
+    private Integer errorCount = 0;
 
     // 未答数
-    private @NonNull
-    Integer noAnswerCount = 0;
+    private Integer noAnswerCount = 0;
 }
