9f84d3d01174b540cd6279170070be0684e63e61
浩进 朱 <3191955858@qq.com>
2025-06-27 13:59:30 +0800

    运动员测试类。

---
+++ src/test/java/com/jesse/routerfunc/Player.java
@@ -0,0 +1,14 @@
+package com.jesse.routerfunc;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Player
+{
+    private String firstName;
+    private String lastName;
+}
