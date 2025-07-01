18835e0750993a31bccd1897bb2199cde2cb732b
浩进 朱 <3191955858@qq.com>
2025-07-01 17:05:05 +0800

    根组件

---
+++ Client/src/App.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+	<div id="app">
+		<main>
+			<router-view></router-view> 
+		</main>
+	</div>
+</template>
+
+<style>
+	#app {
+		font-family: Arial, sans-serif;
+		color: #333;
+	}
+	
+	main { padding: 2rem; }
+</style>
