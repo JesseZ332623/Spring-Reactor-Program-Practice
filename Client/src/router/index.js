3fd39016afb46860d152f9ea680d3681f80ad1d4
浩进 朱 <3191955858@qq.com>
2025-07-02 17:44:58 +0800

    路由属性配置脚本

--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -1,33 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import SingleScoreRecordQuery from '../components/SingleScoreRecordQuery.vue'
 import Hello from '../components/Hello.vue';
+import NotFound from '../components/NotFound.vue';
+import SingleScoreRecordQuery from '../components/SingleScoreRecordQuery.vue'
+import PaginationScoreRecordQuery from '../components/PaginationScoreRecordQuery.vue';
 
 /** 配置路由属性。 */
 const routes = [
 	{
-		path: '/record/:id',     // 对这个路径进行路由
+		path: '/record/:id',     			// 对这个路径进行路由
 		name: SingleScoreRecordQuery,
 		component: SingleScoreRecordQuery,  // 使用 ScoreRecord 组件执行该请求
-		props: true,			 // 需要通过 URL 传递参数
-		meta: {
-			title: "Score"
-		}
+		props: true,			 			// 需要通过 URL 传递参数
+		meta: { title: "Score" }
 	},
 	{
-		path: '/record/',
+		path: '/pagination_record/:page',
+		name: PaginationScoreRecordQuery,
+		component: PaginationScoreRecordQuery,
+		props: true,
+		meta : { title: "Pagination" }
+	},
+	{
+		path: '/record/',			// 对于未填参数的 URL，重定向回默认值
 		redirect: '/record/1'
 	},
 	{
-		path: '/record/:id(\\d+)', // 只匹配数字ID
-		component: SingleScoreRecordQuery,
-		props: true
-    },
+		path: '/pagination_record/',
+		redirect: '/pagination_record/1'
+	},
 	{
 		path: '/',
 		name: Hello,
 		component: Hello,
-		meta: { title: "Welcome" }
+		meta: { title: "Welcome!" }
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		name: NotFound,
+		component: NotFound,
+		meta: { title: "404 NOT FOUND!" }
 	}
 ];
 
