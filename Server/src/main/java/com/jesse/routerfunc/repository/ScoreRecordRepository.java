6d9106df51cd1862d8b721d7e1e7067e1f432fee
浩进 朱 <3191955858@qq.com>
2025-07-03 17:57:11 +0800

    成绩数据表仓储类。（重构分页查询逻辑）

--- Server/src/main/java/com/jesse/routerfunc/repository/ScoreRecordRepository.java
+++ Server/src/main/java/com/jesse/routerfunc/repository/ScoreRecordRepository.java
@@ -38,25 +38,42 @@ public interface ScoreRecordRepository
     Flux<ScoreQueryDTO>
     findRecentScoreRecord(Integer n);
 
-    /** 分页查询成绩。*/
+    /**
+     * 分页查询成绩。
+     *
+     * @param userName 查询用户名
+     * @param limit    每页几条数据？
+     * @param offset   偏移量，也可理解为第几页
+     */
     @Query(value =
         """
         SELECT score_id, user_id, user_name, submit_date,
                    correct_count, error_count, no_answer_count
         FROM score_record
         INNER JOIN users USING(user_id)
+        WHERE user_name = :userName
         ORDER BY submit_date DESC
         LIMIT :limit OFFSET :offset
         """)
     Flux<ScoreQueryDTO>
     findScoreRecordWithPagination(
-        @Param(value = "limit")  int limit,
-        @Param(value = "offset") int offset
+        @Param(value = "userName") String userName,
+        @Param(value = "limit")    int limit,
+        @Param(value = "offset")   int offset
     );
 
     /** 获取总记录数（用于分页） */
-    @Query("SELECT COUNT(*) FROM score_record")
-    Mono<Long> countAllScoreRecords();
+    @Query(
+        """
+        SELECT COUNT(*)
+        FROM score_record
+        INNER JOIN users USING(user_id)
+        WHERE user_name = :userName
+        """)
+    Mono<Long>
+    countAllScoreRecordsByName(
+        @Param(value = "userName") String userName
+    );
 
     /** 清空整张成绩表。*/
     @Query(value = "TRUNCATE score_record")
