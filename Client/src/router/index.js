15c24fec27a27250d5f1fde598f982bc9a9983cf
浩进 朱 <3191955858@qq.com>
2025-07-04 18:02:19 +0800

    路由属性配置脚本。

--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Hello from '../components/Hello.vue';
-import NotFound from '../components/NotFound.vue';
-import SingleScoreRecordQuery from '../components/SingleScoreRecordQuery.vue'
-import PaginationScoreRecordQuery from '../components/PaginationScoreRecordQuery.vue';
+import Hello 				  	   from '../components/Hello.vue';
+import NotFound 				   from '../components/NotFound.vue';
+import SingleScoreRecordQuery 	   from '../components/SingleScoreRecordQuery.vue'
+import PaginationScoreRecordQuery  from '../components/PaginationScoreRecordQuery.vue';
+import AppendNewScore 			   from '../components/AppendNewScore.vue';
 
 /** 配置路由属性。 */
 const routes = [
@@ -12,14 +13,19 @@ const routes = [
 		name: SingleScoreRecordQuery,
 		component: SingleScoreRecordQuery,  // 使用 ScoreRecord 组件执行该请求
 		props: true,			 			// 需要通过 URL 传递参数
-		meta: { title: "Score" }
+		meta: { title: "Single Score Query" }
 	},
 	{
 		path: '/pagination_record/',
 		name: PaginationScoreRecordQuery,
 		component: PaginationScoreRecordQuery,
-		props: true,
-		meta : { title: "Pagination" }
+		meta : { title: "Score Pagination Query" }
+	},
+	{
+		path: '/append_new_score/',
+		name: AppendNewScore,
+		component: AppendNewScore,
+		meta : { title: "Append New Score" }
 	},
 	{
 		path: '/record/',			// 对于未填参数的 URL，重定向回默认值
@@ -32,6 +38,7 @@ const routes = [
 		meta: { title: "Welcome!" }
 	},
 	{
+		// 对于上述路径之外的路径，重定向至 404 页面
 		path: '/:pathMatch(.*)*',
 		name: NotFound,
 		component: NotFound,
