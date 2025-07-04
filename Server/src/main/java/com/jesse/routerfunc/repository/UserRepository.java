75f8c2d4503af4816ad6b732eada42e1966216ef
浩进 朱 <3191955858@qq.com>
2025-07-04 10:03:53 +0800

    用户实体仓储类。

--- Server/src/main/java/com/jesse/routerfunc/repository/UserRepository.java
+++ Server/src/main/java/com/jesse/routerfunc/repository/UserRepository.java
@@ -1,7 +1,14 @@
 package com.jesse.routerfunc.repository;
 
 import com.jesse.routerfunc.entity.UserEntity;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface UserRepository
-    extends ReactiveCrudRepository<UserEntity, Long> {}
+    extends ReactiveCrudRepository<UserEntity, Long>
+{
+    /** 查询用户表中的所有用户名。 */
+    @Query(value = "SELECT user_name FROM users")
+    Flux<String> findAllUserName();
+}
